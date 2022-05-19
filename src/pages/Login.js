908fdce4eb4d71be68b125c61ceccb41f32cea33
Gabriel <gabrielja2antunes@gmail.com>
2022-05-18 21:30:11 -0300

    req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,88 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isButtonDisabled: true,
+      isLoading: false,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.validarInputName = this.validarInputName.bind(this);
+    this.chamaCreateUser = this.chamaCreateUser.bind(this);
+  }
+
+  onInputChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState(
+      {
+        [name]: value,
+      },
+      () => this.validarInputName(),
+    );
+  }
+
+  validarInputName = () => {
+    const { name } = this.state;
+    const minInputNameLength = 3;
+
+    if (name.length < minInputNameLength) {
+      return this.setState({
+        isButtonDisabled: true,
+      });
+    }
+    return this.setState({
+      isButtonDisabled: false,
+    });
+  };
+
+  chamaCreateUser = async () => {
+    const { name } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    const { history } = this.props;
+    history.push('search');
+  }
+
   render() {
-    console.log(this.props);
+    const { isButtonDisabled, name, isLoading } = this.state;
     return (
       <div data-testid="page-login">
-        Olá aqui é o login
+        { isLoading ? <p>Carregando...</p> : (
+          <form>
+            <label htmlFor="login-name-input">
+              Digite seu nome:
+              <input
+                data-testid="login-name-input"
+                className="input-text"
+                onChange={ this.onInputChange }
+                name="name"
+                type="text"
+                value={ name }
+              />
+            </label>
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              onClick={ this.chamaCreateUser }
+              disabled={ isButtonDisabled }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
       </div>
 
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+};
 
 export default Login;
